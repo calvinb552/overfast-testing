60f4017fa513130ef00dd86a3a7ee1eded2c2741
Calvin <CalvinB552@gmail.com>
2024-12-29 14:11:44 -0500

    made more working functions

--- player.py
+++ player.py
@@ -12,9 +12,22 @@ def get_player(username):
 
     return player_ids[0]
 
-def get_player_stats(player_id):
-    player_id = get_player('litapollo')
-    stats_url = 'https://overfast-api.tekrop.fr/players/litapollo-1843/summary'
+def get_player_stats(username):
+    player_id = get_player(username)
+    stats_url = 'https://overfast-api.tekrop.fr/players/' + player_id + '/summary'
     response = requests.get(stats_url)
     stats = response.json()
     return(stats)
+
+def get_player_summary(username):
+    player_id = get_player(username)
+    summary_url = 'https://overfast-api.tekrop.fr/players/'+ player_id +'/stats/summary'
+    response = requests.get(summary_url)
+    return(response)
+
+def get_stats_summary(username, gamemode):
+    player_id = get_player(username)
+    parameters = {"gamemode" : gamemode}
+    stat_sum_url = 'https://overfast-api.tekrop.fr/players/'+ player_id +'/stats/career'
+    response = requests.get(stat_sum_url, params= parameters)
+    return(response.json())
