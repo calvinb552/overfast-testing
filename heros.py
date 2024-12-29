ca68433afa4e02a19a4a9d2a99b7d10dce2fa12a
Calvin <CalvinB552@gmail.com>
2024-12-29 15:42:29 -0500

    hero data

---
+++ heros.py
@@ -0,0 +1,26 @@
+import requests
+
+def get_heros():
+    hero_url = 'https://overfast-api.tekrop.fr/heroes'
+    response = requests.get(hero_url)
+    data = response.json()
+    return data
+
+def get_hero_data(hero):
+    hero_data_url = 'https://overfast-api.tekrop.fr/heroes/'+ hero.lower()
+    response = requests.get(hero_data_url)
+    hero_data = response.json()
+
+    return hero_data
+
+def get_roles():
+    roles_url = 'https://overfast-api.tekrop.fr/roles'
+    response = requests.get(roles_url)
+    roles_data = response.json()
+    return roles_data
+
+
+
+
+
+
