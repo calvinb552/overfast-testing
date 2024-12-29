336d1891c6f05b1e915b1568701c34a51e93a8cc
Calvin <CalvinB552@gmail.com>
2024-12-29 13:44:17 -0500

    making some basic api call functions

--- player.py
+++ player.py
@@ -1,4 +1,20 @@
+import requests
+
+
 def get_player(username):
-    return 0
+    base_url = "https://overfast-api.tekrop.fr/players"
+    parameters = {"name": username}
+    response = requests.get(base_url, params=parameters)
+    data = response.json()
+    player_ids = []
+    for p in data['results']:
+        player_ids.append(p['player_id'])
 
+    return player_ids[0]
 
+def get_player_stats(player_id):
+    player_id = get_player('litapollo')
+    stats_url = 'https://overfast-api.tekrop.fr/players/litapollo-1843/summary'
+    response = requests.get(stats_url)
+    stats = response.json()
+    return(stats)
