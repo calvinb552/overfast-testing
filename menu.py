ec555b3f905492f61de38ac5cfb6c86c9966891c
Calvin <CalvinB552@gmail.com>
2025-01-02 14:03:05 -0500

    main menu finished

--- menu.py
+++ menu.py
@@ -11,6 +11,8 @@ def menu():
             break #exit
         elif choice == 1:
             player_menu()
+        elif choice == 2:
+            overwatch_menu()
         else:
             print('invalid')
 
@@ -33,7 +35,8 @@ def player_menu():
             print(get_player_stats(username))
         elif choice == 2:
             username = input('what is the username of the player you want to find ')
-            print(get_player_summary(username))
+            gamemode = input('what gamemode would you like to get a summary for (casual or competitive)')
+            print(get_player_summary(username, gamemode))
 
 def print_player_menu():
     print('\n=== Overwatch Stats ===')
@@ -41,3 +44,36 @@ def print_player_menu():
     print("0. Main Menu")
     print('1. find players stats')
     print('2. get players summary')
+
+def overwatch_menu():
+    print_overwatch_menu()
+    while True:
+        print_overwatch_menu()
+        choice = int(input('select an option:'))
+        if choice == 0:
+            break
+        elif choice == 1:
+            print(get_heros())
+        elif choice == 2:
+            hero = input('what hero would you like info for?')
+            print(get_hero_data(hero))
+        elif choice == 3:
+            print(get_roles())
+        elif choice == 4:
+            print(gamemodes())
+        elif choice == 5:
+            print(maps())
+        else:
+            print('not a valid input.')
+
+
+
+def print_overwatch_menu():
+    print('\n=== Overwatch Stats ===')
+    print('--- overwatch stats ---')
+    print('0. Main menu')
+    print('1. Get hero list')
+    print('2. Get hero details')
+    print('3. get list of roles')
+    print('4. get list of gamemodes')
+    print('5. get list of maps')
